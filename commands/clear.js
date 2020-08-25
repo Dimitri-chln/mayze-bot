41bf3b486881d76bea3dee228a73ad23ed73ede5
Dimitri-chln <59447588+Dimitri-chln@users.noreply.github.com>
2020-08-25 18:54:19 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,16 +4,14 @@ module.exports = {
   aliases: ["clean"],
   cooldown: 5,
   args: 1,
-  usage: "<nombre> [\"bot\"|mention/id] [-r <regex>]",
+  usage: "<nombre> [\"bot\"|-r <regex>]",
   execute(message, args) {
     const number = parseInt(args[0], 10);
     if (isNaN(number) || number <= 0 || number > 100) return message.reply("Le premier argument doit être un nombre entier compris entre 1 et 100!");
     message.channel.messages.fetch({"limit": number+1}).then(messages => {
       const userRegex = /<@!?\d{18}>/;
       var messagesToDelete = messages;
-      if (args.length >= 2 && userRegex.test(args[1])) {
-        messagesToDelete = messages.filter(msg => msg.author.id === args[1].replace(/[<@!>]/, ""));
-      } else if (args [1] === "bot") {
+      if (args[1].toLowerCase() === "bot") {
         messagesToDelete = messages.filter(msg => msg.author.bot);
       } else if (args[1] === "-r" && args.length >= 3) {
         const regex = new RegExp(args[2], "i");
