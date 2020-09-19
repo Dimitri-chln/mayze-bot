a2e6047e52e28591b9b38adf77e5099127f06e6e
Glitch (mayze) <none>
2020-09-19 12:12:27 +0000

    😀🌿 Updated with Glitch

--- commands/wishRemove.js
+++ commands/wishRemove.js
@@ -3,12 +3,16 @@ module.exports = {
   description: "Retire le wish d'une série pour Mudae",
   aliases: ["wishRemove", "wr"],
   args: 1,
-  usage: "<série>",
+  usage: "<n° série>",
   execute(message, args) {
     const wishData = message.client.dataRead("wishes.json");
-    const series = args.join(" ");
     var wishlist = wishData[message.author.id] || [];
-    wishlist = wishlist.filter(w => w !== series)
+    const index = parseInt(args[0], 10);
+    if (isNaN(index) || index <= 0 || index > wishlist.length)
+      return message.reply(
+        `l'argument doit être un nombre compris entre 1 et ${wishlist.length}`
+      );
+    wishlist = wishlist.filter((w, i) => i + 1 !== index);
     wishData[message.author.id] = wishlist;
     message.client.dataWrite("wishes.json", wishData);
     message.react("✅");
