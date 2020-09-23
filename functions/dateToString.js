062b5d0fd4d0e61cf8ad54712fac84adb0500b69
Glitch (mayze) <none>
2020-09-23 17:23:37 +0000

    🌽🤑 Updated with Glitch

---
+++ functions/dateToString.js
@@ -0,0 +1,16 @@
+module.exports = function dateToString(date) {
+  const years = Math.floor(date / 31536000);
+  const days = Math.floor((date % 31536000) / 86400);
+  const hours = Math.floor((date % 86400) / 3600);
+  const minutes = Math.floor((date % 3600) / 60);
+  const seconds = Math.floor((date % 60) / 1);
+  const dateString = [years, days, hours, minutes, seconds]
+    .join(":")
+    .replace(
+      /(\d+):(\d+):(\d+):(\d+):(\d+)/,
+      "**$1** ans, **$2** jours, **$3** heures, **$4** minutes, **$5** secondes"
+    )
+    .replace(/\*\*0\*\* \w+(, )?/g, "")
+    .replace(/, (\*\*\d+\*\* \w+)$/, " et $1");
+  return dateString
+};
