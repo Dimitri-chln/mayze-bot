a2ada673a43b02170b548c288e9fc124c6801017
Dimitri-Chln <dimitrichaline4@gmail.com>
2020-10-26 21:00:46 +0100

    Updated anime.js

--- commands/anime.js
+++ commands/anime.js
@@ -9,30 +9,53 @@ module.exports = {
         const dataWrite = require("../functions/dataWrite.js");
         var animes = dataRead("animes.json");
         var userAnimes = animes[message.author.id];
-        if (args[1] === "add") {
-            
-        } else if (args[1] === "remove") {
-            
-        } else {
-            const desc = userAnimes.map(a => `**${a.name}**: ${a.seasons.forEach((s, i) => {
-                    const index = s.indexOf(false);
-                    if (index > -1) {
-                        return `Saison ${i+1} - Épisode ${index+1}`;
+        
+        switch ((args[0] || "").toLowerCase()) {
+            case "info":
+                const input = args.slice(1).join(" ");
+                const anime = userAnimes.find(a => 
+                    a.name.toLowerCase() === input.toLowerCase() ||
+                    new RegExp(a.altNames, "i").test(input)
+                );
+                if (!anime) return message.reply("je n'ai pas trouvé cet animé dans ta liste");
+                var nextEpisode = "∅";
+                if (anime.nextEp) nextEpisode = `\`Saison ${anime.nextEp[0]} - Épisode ${anime.nextEp[1]}\``;
+                message.channel.send({
+                    embed: {
+                        author: {
+                            name: anime.name,
+                            icon_url: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
+                        },
+                        color: "#010101",
+                        description: `${anime.seasons.map((s, i) => `• **Saison ${i+1}**: ${s} épisodes`).join("\n")}\n\n__**Prochain épisode:**__ **${nextEpisode}**`,
+                        footer: {
+                            text: "✨ Mayze ✨"
+                        }
                     }
-                })}`).join("\n");
-            message.channel.send({
-                embed: {
-                    author: {
-                        name: "Parties Among Us en cours",
-                        icon_url: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
-                    },
-                    color: "#010101",
-                    desription: desc,
-                    footer: {
-                        text: "✨ Mayze ✨"
+                });
+                break;
+            default:
+                message.channel.send({
+                    embed: {
+                        author: {
+                            name: `Animés de ${message.author.username}`,
+                            icon_url: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
+                        },
+                        color: "#010101",
+                        description: userAnimes.map(a => {
+                            var string = `• **${a.name}** → `;
+                            if (a.nextEp) {
+                                string += `\`Saison ${a.nextEp[0]} - Épisode ${a.nextEp[1]}\``;
+                            } else {
+                                string += "✅";
+                            };
+                            return string;
+                        }).join("\n"),
+                        footer: {
+                            text: "✨ Mayze ✨"
+                        }
                     }
-                }
-            });
+                });
         };
     }
 };
--- functions/dataRead.js
+++ functions/dataRead.js
@@ -1,5 +1,10 @@
 module.exports = function dataRead(fileName) {
   const fs = require("fs");
-  const file = JSON.parse(fs.readFileSync("database/" + fileName));
-  return file;
+  const fileString = fs.readFileSync("database/" + fileName);
+  try {
+    const file = JSON.parse(fileString);
+    return file;
+  } catch (err) {
+    console.log("Error while trying to parse database/" + fileName, err);
+  };
 };
