923e6041cc4cb0badff5db3edff923ad34878274
Dimitri-Chln <dimitrichaline4@gmail.com>
2020-11-15 11:55:04 +0100

    SQL try

--- commands/test.js
+++ commands/test.js
@@ -2,20 +2,20 @@ module.exports = {
     name: "test",
     description: "Test test",
     aliases: [],
-    cooldown: 0.1,
-    args: 1,
-    usage: "[expression]",
+    cooldown: 1,
+    args: 3,
+    usage: "<pokedex_id> <pokedex_name> <is_shiny>",
     ownerOnly: true,
     async execute(message, args) {
         const databaseSQL = require("../functions/databaseSQL.js");
         try {
-            const res = databaseSQL(`INSERT INTO pokemons (caught_at, caught_by, pokedex_id, pokedex_name, is_shiny) VALUES (${Date.now()}, ${message.author.id}, 330, "flygon", false)`);
+            const res = await databaseSQL(`INSERT INTO pokemons (caught_by, pokedex_id, pokedex_name, is_shiny) VALUES (${message.author.id}, ${args[0]}, ${args[1]}, ${args[2]})`);
             message.channel.send(`La table contient désormais ${res.rowCount} rows`);
         } catch (err) {
             console.log(err);
         }
         try {
-            const res = databaseSQL(`SELECT * FROM pokemons`);
+            const res = await databaseSQL(`SELECT * FROM pokemons`);
             message.channel.send(`\`\`\`js\n${JSON.stringify(res, null, 4)}\n\`\`\``);
         } catch (err) {
             console.log(err);
