0a594302c605f283c36145f0d9e8a6aef1b5cb4a
Dimitri-chln <dimitrichaline4@gmail.com>
2020-11-28 17:49:26 +0100

    Removed a console.log

--- autoresponses/bad-words.js
+++ autoresponses/bad-words.js
@@ -3,7 +3,6 @@ const command = {
 		const badWords = ["plop", "mich", "chocolatine"];
 		if (message.author.bot) return;
 		const badWordsRegex = new RegExp(`(\\b${badWords.join("\\b)|(\\b")}\\b)`, "i");
-		console.log(badWordsRegex)
 		if (badWordsRegex.test(message.content)) {
 			message.delete().catch(console.error);
 			const msg = await message.reply("surveille ton langage").catch(console.error);
