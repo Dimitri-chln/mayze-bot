75e9934a150bb1ca6a2d3a18ff58de7c448544e7
Dimitri-chln <dimitrichaline4@gmail.com>
2020-12-04 23:12:12 +0100

    Added error reply

--- commands/nick.js
+++ commands/nick.js
@@ -1,20 +1,27 @@
+const { Message } = require("discord.js");
+
 const command = {
-    name: "nick",
-    description: "Modifie ou réinitialise ton pseudo sur le serveur",
-    aliases: ["name", "rename"],
-    args: 0,
-    usage: "[pseudo]",
-    perms: ["CHANGE_NICKNAME"],
-    async execute(message, args) {
-        try {
-            message.member.setNickname(args.join(" "));
-            try { message.delete(); }
-            catch (err) { console.log(err); }
-        } catch (err) {
-            try { message.channel.send("Je n'ai pas la permission de changer ton pseudo"); }
-            catch (err) { console.log(err); }
-        }
-    }
+	name: "nick",
+	description: "Modifie ou réinitialise ton pseudo sur le serveur",
+	aliases: ["name", "rename"],
+	args: 0,
+	usage: "[pseudo]",
+	perms: ["CHANGE_NICKNAME"],
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
+	async execute(message, args) {
+		message.delete().catch(console.error);
+		message.member.setNickname(args.join(" ")).catch(async err => {
+			if (err.message === "Missing Permissions") {
+				const msg = await message.reply("je n'ai pas la permission de changer ton pseudo").catch(console.error);
+				msg.delete({ timeout: 5000 }).catch(console.error);
+				return;
+			}
+			console.error(err);
+		});
+	}
 };
 
 module.exports = command;
