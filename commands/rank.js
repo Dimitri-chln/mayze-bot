ee95fb30a6af57990e9fc8f9799c71c0fcba03bc
Dimitri-chln <dimitrichaline4@gmail.com>
2020-12-19 15:46:16 +0100

    Merged *rank and *ranks

--- commands/rank.js
+++ commands/rank.js
@@ -1,43 +1,70 @@
+const { Message } = require("discord.js");
+
 const command = {
 	name: "rank",
 	description: "Rejoins ou quitte un rank",
 	aliases: [],
 	args: 1,
-	usage: "<rank>",
+	usage: "[rank]",
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
 	async execute(message, args) {
-		if (message.member.roles.cache.some(r => r.id === "695943648235487263")) return;
-	
-		const roleTop = message.guild.roles.cache.get("735810286719598634");
-		const roleBottom = message.guild.roles.cache.get("735810462872109156");
-		const ranks = message.guild.roles.cache.filter(r => r.position < roleTop.position && r.position > roleBottom.position && !r.name.includes("(Jailed)"));
-		const rankIdOrName = args.join(" ").toLowerCase();
-		const rank = message.guild.roles.cache.get(rankIdOrName) ||
-			ranks.find(r => r.name.toLowerCase() === rankIdOrName) ||
-			ranks.find(r => r.name.toLowerCase().includes(rankIdOrName));
+		if (args.length) {
+			if (message.member.roles.cache.some(r => r.id === "695943648235487263")) return;
+		
+			const roleTop = message.guild.roles.cache.get("735810286719598634");
+			const roleBottom = message.guild.roles.cache.get("735810462872109156");
+			const ranks = message.guild.roles.cache.filter(r => r.position < roleTop.position && r.position > roleBottom.position && !r.name.includes("(Jailed)"));
+			const rankIdOrName = args.join(" ").toLowerCase();
+			const rank = message.guild.roles.cache.get(rankIdOrName) ||
+				ranks.find(r => r.name.toLowerCase() === rankIdOrName) ||
+				ranks.find(r => r.name.toLowerCase().includes(rankIdOrName));
 
-		if (!ranks.array().includes(rank)) {
-			return message.reply("ce rank n'existe pas").catch(console.error);
-		}
+			if (!ranks.array().includes(rank)) {
+				return message.reply("ce rank n'existe pas").catch(console.error);
+			}
 
-		if (!message.member.roles.cache.has(rank.id)) {
-			try {
-				await message.member.roles.add(rank);
-				message.channel.send(`${message.author} a rejoint le rank \`${rank.name}\``);
-			} catch (err) {
-				console.log(err);
-				try{ message.channel.send("Quelque chose s'est mal passé en te donnant le rôle :/"); }
-				catch (err) { console.log(err); }
+			if (!message.member.roles.cache.has(rank.id)) {
+				try {
+					await message.member.roles.add(rank);
+					message.channel.send(`${message.author} a rejoint le rank \`${rank.name}\``).catch(console.error);
+				} catch (err) {
+					console.log(err);
+					message.channel.send("Quelque chose s'est mal passé en te donnant le rôle :/").catch(console.error);
+				}
 			}
-		}
-		if (message.member.roles.cache.has(rank.id)) {
-			try {
-				await message.member.roles.remove(rank);
-				message.channel.send(`${message.author} a quitté le rank \`${rank.name}\``);
-			} catch (err) {
-				console.log(err);
-				try{ message.channel.send("Quelque chose s'est mal passé en te retirant le rôle :/"); }
-				catch (err) { console.log(err); }
+			if (message.member.roles.cache.has(rank.id)) {
+				try {
+					await message.member.roles.remove(rank);
+					message.channel.send(`${message.author} a quitté le rank \`${rank.name}\``).catch(console.error);
+				} catch (err) {
+					console.log(err);
+					message.channel.send("Quelque chose s'est mal passé en te retirant le rôle :/").catch(console.error);
+				}
 			}
+		} else {
+			const roleTop = message.guild.roles.cache.get("735810286719598634");
+			const roleBottom = message.guild.roles.cache.get("735810462872109156");
+			const ranks = message.guild.roles.cache.filter(r => r.position < roleTop.position && r.position > roleBottom.position && !r.name.includes("(Jailed)"));
+			message.channel.send({
+				embed: {
+					author: {
+					name: "Ranks du serveur 🎗️",
+					icon_url: message.client.user.avatarURL()
+					},
+					color: "#010101",
+					description: ranks.map(rank => {
+						var hasRank;
+						if (message.member.roles.cache.has(rank.id)) hasRank = "✅";
+						return `• ${rank} | ${hasRank}`;
+					}).join("\n"),
+					footer: {
+					text: "✨Mayze✨"
+					}
+				}
+			}).catch(console.error);
 		}
 	}
 };
