59e83bf6c14a46b0a9b976fd2d38f6813c5b9ee9
Dimitri-chln <dimitrichaline4@gmail.com>
2020-12-20 12:03:22 +0100

    .

--- modules/timeToString.js
+++ modules/timeToString.js
@@ -7,14 +7,14 @@ function timeToString(date) {
 	const hours = Math.floor((date % 86400) / 3600);
 	const minutes = Math.floor((date % 3600) / 60);
 	const seconds = Math.floor((date % 60) / 1);
-	const dateString = [years, days, hours, minutes, seconds]
+	const timeString = [years, days, hours, minutes, seconds]
 		.join(":")
 		.replace(/(\d+):(\d+):(\d+):(\d+):(\d+)/,"**$1** ans, **$2** jours, **$3** heures, **$4** minutes, **$5** secondes, ")
 		.replace(/\*\*0\*\* \w+, /g, "")
 		.replace(/(\*\*1\*\* \w+)s/g, "$1")
 		.replace(/, (\*\*\d+\*\* \w+), $/, " et $1")
 		.replace(/^(\*\*\d+\*\* \w+), $/, "$1");
-	return dateString;
+	return timeString;
 };
 
-module.exports = timeToString
+module.exports = timeToString;
