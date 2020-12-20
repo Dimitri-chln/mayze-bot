e706dd23148dea67a760dbce40660d4bd8d23263
Dimitri-chln <dimitrichaline4@gmail.com>
2020-12-20 13:37:30 +0100

    Fixed embed message

--- commands/role-color.js
+++ commands/role-color.js
@@ -1,3 +1,5 @@
+const { Message } = require("discord.js");
+
 const command = {
 	name: "role-color",
 	description: "Change la couleur d'un rôle",
@@ -5,15 +7,18 @@ const command = {
 	args: 2,
 	usage: "<rôle/id> <couleur>",
 	perms: ["MANAGE_ROLES"],
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
 	async execute(message, args) {
 		const roleIdOrName = args[0].toLowerCase();
 		const role =
 			message.guild.roles.cache.get(roleIdOrName) ||
 			message.guild.roles.cache.find(r => r.name.toLowerCase() === roleIdOrName) ||
 			message.guild.roles.cache.find(r => r.name.toLowerCase().includes(roleIdOrName));
-		if (!role) {
-			message.reply("je n'ai pas réussi à trouver ce rôle").catch(console.error);
-		}
+		if (!role) return message.reply("je n'ai pas réussi à trouver ce rôle").catch(console.error);
+		const color = args[1].replace(/#/, "");
 
 		try { await role.setColor(args[1]); }
 		catch (err) {
@@ -23,9 +28,12 @@ const command = {
 		try {
 			message.channel.send({
 				embed: {
-					auhtor: {
+					author: {
 						name: "Couleur modifiée avec succès",
 						icon_url: message.client.user.avatarURL()
+					},
+					thumbnail: {
+						url: `https://dummyimage.com/50/${color}/${color}.png`
 					},
 					color: "#010101",
 					description: `La couleur du rôle ${role} a été changée en ${args[1]}`,
