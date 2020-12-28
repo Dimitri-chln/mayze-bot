e3bcd58d1a68c50786265e3229aae06e04bac532
Dimitri-chln <dimitrichaline4@gmail.com>
2020-12-28 15:33:43 +0100

    Added reaction on command

--- commands/message.js
+++ commands/message.js
@@ -1,3 +1,5 @@
+const { Message } = require("discord.js");
+
 const command = {
 	name: "message",
 	description: "Envoie un message dans un salon",
@@ -5,16 +7,19 @@ const command = {
 	args: 2,
 	usage: "<salon> <texte>",
 	perms: ["MANAGE_MESSAGES"],
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
 	async execute(message, args) {
 		const channel = message.mentions.channels.first();
-		if (!channel) {
-			message.reply("indique le salon dans lequel je dois envoyer le message").catch(console.error);
-		}
-		try { channel.send(args.splice(1).join(" ")); }
-		catch (err) {
-			console.log(err);
-			message.channel.send("Quelque chose s'est mal passé en envoyant le message").catch(console.error);
-		}
+		if (!channel) return message.reply("indique le salon dans lequel je dois envoyer le message").catch(console.error);
+		channel.send(args.splice(1).join(" "))
+		.then(() => message.react("✅"))
+		.catch(err => {
+			console.error(err);
+			message.react("❌");
+		});
 	}
 };
 
