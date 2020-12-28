02c95ff936bc42e1ec62cfc8fd1833c380bddc04
Dimitri-chln <dimitrichaline4@gmail.com>
2020-12-28 16:46:18 +0100

    .

--- autoresponses/mudae-pin.js
+++ autoresponses/mudae-pin.js
@@ -7,7 +7,7 @@ const command = {
 	async execute(message) {
 		if (message.author.id !== "432610292342587392") return;
 		if (message.guild.id !== "689164798264606784") return;
-		const pinChannel = message.guild.channels.cache.get("788428208298000435");
+		const pinChannel = message.client.channels.cache.get("788428208298000435");
 		const legRegex = /Félicitations, vous venez de gagner un\.\.\. Un\.\.\. <:.+:(\d{18})> \*\*(\w+)\*\*\?!/;
 		const ubRegex =/un <:.+:(\d{18})> \*\*(\w+)\*\*\. Euh, QUOI \?/;
 		const shinyRegex = /<:.+:(\d{18})> \*\*(\w+) <:shinySparkles:653808283244560402>\*\*/;
