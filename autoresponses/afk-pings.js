0dc8f5d992d66e504ccb1d39302862ec43e78445
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-09 13:41:24 +0100

    Added *afk

---
+++ autoresponses/afk-pings.js
@@ -0,0 +1,25 @@
+const { Message } = require("discord.js");
+
+const autoresponse = {
+    /**
+     * @param {Message} message 
+     */
+    async execute(message) {
+        const timeToString = require("../modules/timeToString.js");
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+        const mentionned = message.mentions.users;
+        const { "rows": afkUsers } = await message.client.pg.query("SELECT * FROM afk").catch(console.error);
+        if (!afkUsers) return;
+        afkUsers.forEach(u => {
+            if (mentionned.has(u.user_id)) message.channel.send(`**${mentionned.get(u.user_id).username}** est AFK depuis ${timeToString(Math.floor((Date.now() - Date.parse(u.time)) / 1000))}${u.message ? ` : **${u.message}**` : ""} 💤`).catch(console.error);
+            if (message.author.id === u.user_id && Date.now() - Date.parse(u.time) > 60000) {
+                message.client.pg.query(`DELETE FROM afk WHERE user_id = '${message.author.id}'`).catch(console.error);
+                message.react("👋").catch(console.error);
+                setTimeout(() => message.reactions.cache.get("👋").remove().catch(console.error), 4000);
+            }
+        });
+    }
+};
+
+module.exports = autoresponse;
---
+++ commands/afk.js
@@ -0,0 +1,24 @@
+const { Message } = require("discord.js");
+
+const command = {
+	name: "afk",
+	description: "Te mets AFK pour prévenir les autres membres",
+	aliases: [],
+	args: 0,
+	usage: "[message]",
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
+	async execute(message, args) {
+		const AKFmessage = args.join(" ").replace(/^./, a => a.toUpperCase());
+		const { rows } = await message.client.pg.query(`SELECT * FROM afk WHERE user_id = '${message.author.id}'`).catch(console.error);
+		if (rows.length) message.client.pg.query(`DELETE FROM afk WHERE user_id = '${message.author.id}'`).catch(console.error);
+		if (AKFmessage) message.client.pg.query(`INSERT INTO afk (user_id, message) VALUES ('${message.author.id}', '${AKFmessage}')`).catch(console.error);
+		else message.client.pg.query(`INSERT INTO afk (user_id) VALUES ('${message.author.id}')`).catch(console.error);
+
+		message.channel.send(`> ${message.author} est maintenant AFK 💤\n${AKFmessage ? `**→ ${AKFmessage}**` : ""}`).catch(console.error);
+	}
+};
+
+module.exports = command;
