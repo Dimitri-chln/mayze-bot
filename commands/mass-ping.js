fb18c613e80d813a69cf67469a4fd7ec2a1f783d
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-10 09:50:46 +0100

    Messages are now deleted in bulk

--- commands/mass-ping.js
+++ commands/mass-ping.js
@@ -1,3 +1,5 @@
+const { Message } = require("discord.js");
+
 const command = {
 	name: "mass-ping",
 	description: "Ping une personne en boucle",
@@ -5,20 +7,24 @@ const command = {
 	args: 1,
 	usage: "<mention> [nombre]",
 	perms: ["MANAGE_MESSAGES"],
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
 	async execute(message, args) {
+		const { Collection } = require("discord.js");
+		const messages = new Collection();
 		const user = message.mentions.users.first();
 		const n = parseInt(args[1], 10) || 10;
-		message.delete();
-		if (isNaN(n) || n < 0 || n > 100) {
-			message.reply("le nombre doit être compris entre 0 et 100").catch(console.error);
-		}
+		message.delete().catch(console.error);
+		if (isNaN(n) || n < 0 || n > 100) return message.reply("le nombre doit être compris entre 0 et 100").catch(console.error);
+		
 		for (i = 0; i < n; i++) {
-			try {
-				const msg = await message.channel.send(`${user}`);
-				msg.delete();
-			}
-			catch (err) { console.log(err); }
-		};
+			const msg = await message.channel.send(`${user}`).catch(console.error);
+			messages.set(msg.id, msg);
+		}
+
+		message.channel.bulkDelete(messages).catch(console.error);
 	}
 };
 
