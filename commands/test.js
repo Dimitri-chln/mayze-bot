baca8a85f0eb3e2e757f8ba5047ad36d4ce9d2e9
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-11 18:56:27 +0100

    Update *pokemons

--- commands/test.js
+++ commands/test.js
@@ -13,9 +13,21 @@ const command = {
 	 * @param {string[]} args 
 	 */
 	async execute(message, args) {
-		const fs = require("fs");
-		const { rows } = await message.client.pg.query("SELECT * FROM pokemons").catch(console.error);
-		fs.writeFile("backups/database_1.json", JSON.stringify(rows, null, 4), "utf8", () => console.log("Backup complete"));
+		return;
+		const pokedex = require("oakdex-pokedex");
+		const backup = require("../backups/database_pokemons.json");
+		getPokemons(backup);
+
+		async function getPokemons(data) {
+			if (!data.length) return console.log("Transfer complete");
+			const pkm = data[0];
+			const legendaries = ["Artikodin", "Électhor", "Sulfura", "Mewtwo", "Mew", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Celebi", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Créhelf", "Créfollet", "Créfadet", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Cobaltium", "Terrakium", "Viridium", "Boréas", "Fulguris", "Reshiram", "Zekrom", "Démétéros", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Type:0", "Silvallié", "Tokorico", "Tokopiyon", "Tokotoro", "Tokopisco", "Cosmog", "Cosmovum", "Solgaleo", "Lunala", "Zéroïd", "Mouscoto", "Cancrelove", "Cäblifère", "Bamboiselle", "Katagami", "Engloutyran", "Necrozma", "Magearna", "Marshadow", "Vémini", "Mandrillon", "Ama-Ama", "Pierroteknik", "Zeraora", "Meltan", "Melmetal"];
+			const is_legendary = legendaries.includes(pkm.pokedex_name) ? true : false;
+			const count = backup.filter(p => p.pokedex_id === pkm.pokedex_id && p.is_shiny === pkm.is_shiny).length;
+			const newData = data.filter(p => p.pokedex_id !== pkm.pokedex_id || p.is_shiny !== pkm.is_shiny);
+			await message.client.pg.query(`INSERT INTO pokemons (user_id, pokedex_id, pokedex_name, caught, shiny, legendary) VALUES ('${pkm.caught_by}', ${pkm.pokedex_id}, '${pokedex.findPokemon(pkm.pokedex_id).names.en.replace(/'/g, "U+0027")}', ${count}, ${pkm.is_shiny}, ${is_legendary})`).catch(console.error);
+			getPokemons(newData);
+		}
 	}
 };
 
