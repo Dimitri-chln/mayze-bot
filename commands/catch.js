1975323741ad8110a57e320b6dc64792cc1a1a59
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-11 17:15:30 +0100

    Updated pokemons

--- commands/catch.js
+++ commands/catch.js
@@ -12,9 +12,10 @@ const command = {
 	 * @param {string[]} _args 
 	 */
 	async execute(message, _args) {
-		return message.reply("cette commande est désactivée pour le moment").catch(console.error);
+		// return message.reply("cette commande est désactivée pour le moment").catch(console.error);
 
 		const pokedex = require("oakdex-pokedex");
+		const { pokeball } = require("../assets/misc.json");
 
 		const shinyFrequency = 0.004, alolanFrequency = 0.1, galarianFrequency = 0.1;
 		const { catchRates } = message.client;
@@ -22,7 +23,7 @@ const command = {
 		const random = Math.random() * (catchRates.slice(-1)[0] + pokedex.findPokemon(catchRates.length).catch_rate);
 		const pokemon = findDrop(random);
 		
-		var img = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(`00${pokemon.national_id}`).substr(-3)}.png`;
+		let img = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(`00${pokemon.national_id}`).substr(-3)}.png`;
 
 		/*
 		var randomAlolan = Math.random(), alolanText = "";
@@ -34,44 +35,35 @@ const command = {
 		if (randomGalarian < galarianFrequency && pokemon.galarian && alolanText === "") {
 			galarianText = " de Galar";
 			img = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.galarian}.png`;
-		}
-		*/
-		var randomShiny = Math.random(), shinyText = "", is_shiny = false, embedColor = "#010101";
+		} */
+
+		let randomShiny = Math.random(), shiny = false;
 		if (randomShiny < shinyFrequency) {
-			shinyText = "⭐ ";
-			is_shiny = true;
-			embedColor = "#ddbb20";
+			shiny = true;
 			img = `https://img.pokemondb.net/sprites/home/shiny/${pokemon.names.en.toLowerCase()}.png`;
 		}
 
-		const pokemonName = pokemon.names.fr;
-		
-		try {
-			await message.client.pg.query(`INSERT INTO pokemons (caught_by, pokedex_id, pokedex_name, is_shiny) VALUES ('${message.author.id}', ${pokemon.national_id}, '${pokemonName.replace(/'/g, "U+0027")}', ${is_shiny})`);
-		} catch (err) {
-			console.log(err);
-			return message.channel.send("Quelque chose s'est mal passé en joignant la base de données :/").catch(console.error);
+		const legendaries = ["Artikodin", "Électhor", "Sulfura", "Mewtwo", "Mew", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Celebi", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Créhelf", "Créfollet", "Créfadet", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Cobaltium", "Terrakium", "Viridium", "Boréas", "Fulguris", "Reshiram", "Zekrom", "Démétéros", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Type:0", "Silvallié", "Tokorico", "Tokopiyon", "Tokotoro", "Tokopisco", "Cosmog", "Cosmovum", "Solgaleo", "Lunala", "Zéroïd", "Mouscoto", "Cancrelove", "Cäblifère", "Bamboiselle", "Katagami", "Engloutyran", "Necrozma", "Magearna", "Marshadow", "Vémini", "Mandrillon", "Ama-Ama", "Pierroteknik", "Zeraora", "Meltan", "Melmetal"];
+		const legendary = legendaries.includes(pokemon.names.fr) ? true : false;
+
+		const { rows } = await message.client.pg.query(`SELECT * FROM pokemons WHERE user_id = '${message.author.id}' AND pokedex_name = '${pokemon.names.en}' AND shiny = ${shiny}`).catch(console.error);
+		if (rows.length) {
+			message.client.pg.query(`UPDATE pokemons SET caught = ${rows[0].caught + 1} WHERE user_id = '${message.author.id}' AND pokedex_name = '${pokemon.names.en}' AND shiny = ${shiny}`).catch(console.error);
+		} else {
+			message.client.pg.query(`INSERT INTO pokemons (user_id, pokedex_id, pokedex_name, shiny, legendary) VALUES ('${message.author.id}', ${pokemon.national_id}, '${pokemon.names.en}', ${shiny}, ${legendary})`).catch(console.error);
 		}
-		
-		var author = "Pokémon capturé!";
-		try {
-			const { rows } = await message.client.pg.query(`SELECT * FROM pokemons WHERE caught_by='${message.author.id}' AND pokedex_name='${pokemonName}'`);
-			if (rows.length === 1) {
-				author = "Nouveau pokémon! 🎗️";
-			}
-		} catch (err) { console.log(err); }
 
 		message.channel.send({
 			embed: {
 				author: {
-					name: author,
-					icon_url: "https://i.imgur.com/uJlfMAd.png"
+					name: rows.length ? "Pokémon capturé !" : "Nouveau pokémon ! 🎗️",
+					icon_url: pokeball
 				},
 				image: {
 					url: img
 				},
-				color: embedColor,
-				description: `${message.author} a attrapé un ${shinyText}${pokemonName} !`,
+				color: shiny ? "#ddbb20" : "#010101",
+				description: `${message.author} a attrapé un ${shiny ? "⭐ " : ""}${pokemon.names.fr} !`,
 				footer: {
 					text: "✨Mayze✨",
 					icon_url: message.author.avatarURL({ dynamic: true })
@@ -83,9 +75,8 @@ const command = {
 		 * @param {number} random A random number
 		 */
 		function findDrop(random) {
-			for (i = 0; i < catchRates.length; i ++) {
+			for (i = 0; i < catchRates.length; i ++)
 				if (random < catchRates[i]) return pokedex.findPokemon(i);
-			}
 			return pokedex.findPokemon(catchRates.length);
 		}
 	}
--- commands/pokemon.js
+++ commands/pokemon.js
@@ -12,44 +12,30 @@ const command = {
 	 */
 	async execute(message, args) {
 		const { MessageEmbed } = require("discord.js");
+		const pokedex = require("oakdex-pokedex");
 		const pagination = require("../modules/pagination.js");
-		var pokemons;
-		try {
-			const { rows }  = await message.client.pg.query(`SELECT * FROM pokemons WHERE caught_by='${message.author.id}' ORDER BY id`);
-			pokemons = rows;
-		} catch (err) {
-			console.log(err);
-			return message.channel.send("Quelque chose s'est mal passé en joignant la base de données").catch(console.error);
-		}
 
-		if (args.includes("-legendary")) {
-			const legendaries =  ["Artikodin", "Électhor", "Sulfura", "Mewtwo", "Mew", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Celebi", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Créhelf", "Créfollet", "Créfadet", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Cobaltium", "Terrakium", "Viridium", "Boréas", "Fulguris", "Reshiram", "Zekrom", "Démétéros", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Type:0", "Silvallié", "Tokorico", "Tokopiyon", "Tokotoro", "Tokopisco", "Cosmog", "Cosmovum", "Solgaleo", "Lunala", "Zéroïd", "Mouscoto", "Cancrelove", "Cäblifère", "Bamboiselle", "Katagami", "Engloutyran", "Necrozma", "Magearna", "Marshadow", "Vémini", "Mandrillon", "Ama-Ama", "Pierroteknik", "Zeraora", "Meltan", "Melmetal"];
-			pokemons = pokemons.filter(p => legendaries.includes(p.pokedex_name));
-		}
-		if (args.includes("-shiny")) {
-			pokemons = pokemons.filter(p => p.is_shiny);
-		};
-		if (args.includes("-alolan")) {
-			pokemons = pokemons.filter(p => p.pokedex_name.includes("dU+0027Alola"));
-		};
-		if (args.includes("-galarian")) {
-			pokemons = pokemons.filter(p => p.pokedex_name.includes("de Galar"));
-		};
+		const { "rows": pokemons }  = await message.client.pg.query(`SELECT * FROM pokemons WHERE user_id = '${message.author.id}' ORDER BY caught DESC`).catch(console.error);
+		if (!pokemons) return message.channel.send("Quelque chose s'est mal passé en accédant à la base de données").catch(console.error);
+
+		if (args.includes("-legendary")) pokemons = pokemons.filter(p => p.legendary);
+		if (args.includes("-shiny")) pokemons = pokemons.filter(p => p.shiny);
+		if (args.includes("-alolan")) pokemons = pokemons.filter(p => p.pokedex_name.includes("dU+0027Alola"));
+		if (args.includes("-galarian")) pokemons = pokemons.filter(p => p.pokedex_name.includes("de Galar"));
 		
 		const pkmPerPage = 15;
-		var pages = [];
-		var embed = new MessageEmbed()
+		let pages = [];
+		let embed = new MessageEmbed()
 			.setAuthor(`Pokémons de ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
 			.setColor("#010101")
 			.setDescription("*Aucun pokémon ne correspond à la recherche*");
-		if (!pokemons.length) {
-			pages.push(embed);
-		};
+		if (!pokemons.length) pages.push(embed);
+
 		for (i = 0; i < pokemons.length; i += pkmPerPage) {
 			embed = new MessageEmbed()
 				.setAuthor(`Pokémons de ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
 				.setColor("#010101")
-				.setDescription(pokemons.slice(i, i + pkmPerPage).map(p => { if (p.is_shiny) return `⭐ **${p.pokedex_name.replace(/U\+0027/g, "'")}** | ID: ${p.id}`; return `**${p.pokedex_name.replace(/U\+0027/g, "'")}** | ID: ${p.id}` }).join("\n"));
+				.setDescription(pokemons.slice(i, i + pkmPerPage).map(p => `**${pokedex.findPokemon(p.pokedex_id).names.fr}** - ${p.caught} attrapé${p.caught > 1 ? "s" : ""}`).join("\n"));
 			pages.push(embed);
 		};
 		
