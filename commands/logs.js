2870ea73366d1efc24f3cd48a03485d5d8677d8f
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-12 11:35:33 +0100

    Added *logs

---
+++ commands/logs.js
@@ -0,0 +1,51 @@
+const { Message } = require("discord.js");
+
+const command = {
+	name: "logs",
+	description: "Regarde les logs récent du bot",
+	aliases: [],
+	args: 0,
+	usage: "",
+	ownerOnly: true,
+	/**
+	 * @param {Message} message 
+	 * @param {string[]} args 
+	 */
+	async execute(message, args) {
+		if (process.env.HOST !== "HEROKU") return message.reply("cette commande ne fonctionne que sur Heroku").catch(console.error);
+		const shellExec = require("../modules/shellExec.js");
+		const output = shellExec("heroku logs --app mayze-bot");
+		const charactersPerPage = 2000;
+		if (output.length < charactersPerPage) {
+			message.channel.send({
+				embed: {
+					author: {
+						name: "Logs Heroku",
+						icon_url: message.author.avatarURL({ dynamic: true })
+					},
+					color: "#010101",
+					description: `\`\`\`\n${resString}\n\`\`\``,
+					footer: {
+						text: "✨Mayze✨"
+					}
+				}
+			}).catch(console.error);
+		} else {
+			const pagination = require("../modules/pagination.js");
+			const { MessageEmbed } = require("discord.js");
+			const regex = /\[?\s*\{\n(.|\n){0,2000}\},?\n\]?/yg;
+			const matches = output.match(regex);
+			let pages = [];
+			for (i = 0; i < matches.length; i++) {;
+				let embed = new MessageEmbed()
+					.setColor("#010101")
+					.setAuthor("Logs Heroku", message.author.avatarURL({ dynamic: true }))
+					.setDescription(`\`\`\`\n${matches[i]}\n\`\`\``);
+				pages.push(embed);
+			}
+			pagination(message, pages).catch(console.error);
+		}
+	}
+};
+
+module.exports = command;
