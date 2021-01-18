a23632882a8b505d03720058ccd84580045da1bc
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-18 20:34:18 +0100

    Added send-emotes

---
+++ autoresponses/send-emotes.js
@@ -0,0 +1,28 @@
+const { Message } = require("discord.js");
+
+const command = {
+	/**
+	 * @param {Message} message 
+	 */
+	execute: async (message) => {
+		const { webhookID } = require("../config.json");
+		if (message.author.bot || message.guild.id !== "689164798264606784") return;
+
+		const regex = /:[\w-_]+:/g;
+		if (!regex.test(message.content)) return;
+		const emojiNames = message.content.match(regex).map(e => e.replace(/:/g, ""));
+		if (!emojiNames.every(emojiName => message.client.emojis.cache.find(emoji => emoji.name === emojiName))) return;
+
+		const newMsg = message.content.replace(regex, a => message.client.emojis.cache.find(e => e.name === a.replace(/:/g, "")).toString());
+
+		const webhooks = await message.guild.fetchWebhooks().catch(console.error);
+		if (!webhooks) return;
+		const webhook = webhooks.get(webhookID);
+		await webhook.edit({ channel: message.channel.id });
+
+		message.delete().catch(console.error);
+		webhook.send(newMsg, { avatarURL: message.author.avatarURL(), username: message.member.nickname || message.author.username}).catch(console.error);
+	}
+};
+
+module.exports = command;
--- commands/test.js
+++ commands/test.js
@@ -14,29 +14,11 @@ const command = {
 	 * @param {Object[]} options
 	 */
 	execute: async (message, args, options) => {
-		const pokedex = require("oakdex-pokedex");
-		const legendaries = ["Artikodin", "Électhor", "Sulfura", "Mewtwo", "Mew", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Celebi", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Créhelf", "Créfollet", "Créfadet", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Cobaltium", "Terrakium", "Viridium", "Boréas", "Fulguris", "Reshiram", "Zekrom", "Démétéros", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Type:0", "Silvallié", "Tokorico", "Tokopiyon", "Tokotoro", "Tokopisco", "Cosmog", "Cosmovum", "Solgaleo", "Lunala", "Zéroïd", "Mouscoto", "Cancrelove", "Cäblifère", "Bamboiselle", "Katagami", "Engloutyran", "Necrozma", "Magearna", "Marshadow", "Vémini", "Mandrillon", "Ama-Ama", "Pierroteknik", "Zeraora", "Meltan", "Melmetal"];
-		
-		pokedex.allPokemon().sort((a, b) => a.catch_rate - b.catch_rate).forEach(p => {
-			//console.log(`${(`00${p.national_id}`).substr(-3)}. ${p.names.fr} => ${p.catch_rate}`);
-			if (legendaries.includes(p.names.fr)) console.log(`${p.national_id}. ${p.names.fr} => ${p.catch_rate}`);
-		});
-		
-		/*
-		const backup = require("../backups/database_pokemons.json");
-		getPokemons(backup);
-
-		async function getPokemons(data) {
-			if (!data.length) return console.log("Transfer complete");
-			const pkm = data[0];
-			const legendaries = ["Artikodin", "Électhor", "Sulfura", "Mewtwo", "Mew", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Celebi", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Créhelf", "Créfollet", "Créfadet", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Cobaltium", "Terrakium", "Viridium", "Boréas", "Fulguris", "Reshiram", "Zekrom", "Démétéros", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Type:0", "Silvallié", "Tokorico", "Tokopiyon", "Tokotoro", "Tokopisco", "Cosmog", "Cosmovum", "Solgaleo", "Lunala", "Zéroïd", "Mouscoto", "Cancrelove", "Cäblifère", "Bamboiselle", "Katagami", "Engloutyran", "Necrozma", "Magearna", "Marshadow", "Vémini", "Mandrillon", "Ama-Ama", "Pierroteknik", "Zeraora", "Meltan", "Melmetal"];
-			const is_legendary = legendaries.includes(pkm.pokedex_name) ? true : false;
-			const count = backup.filter(p => p.caught_by === pkm.caught_by && p.pokedex_id === pkm.pokedex_id && p.is_shiny === pkm.is_shiny).length;
-			const newData = data.filter(p => p.caught_by !== pkm.caught_by || p.pokedex_id !== pkm.pokedex_id || p.is_shiny !== pkm.is_shiny);
-			await message.client.pg.query(`INSERT INTO pokemons (user_id, pokedex_id, pokedex_name, caught, shiny, legendary) VALUES ('${pkm.caught_by}', ${pkm.pokedex_id}, '${pokedex.findPokemon(pkm.pokedex_id).names.en.replace(/'/g, "U+0027")}', ${count}, ${pkm.is_shiny}, ${is_legendary})`).catch(console.error);
-			getPokemons(newData);
-		}
-		*/
+		// https://discord.com/api/webhooks/800791146456023111/RBX84XUa7V5ni-JSMkrHGYZKimO4VsRn5XGl2HGMXUoM65xo7_gGKNMyi6N5xUtNvbjQ
+		const webhooks = await message.guild.fetchWebhooks();
+		const webhook = webhooks.get("800791146456023111");
+		await webhook.edit({ channel: message.channel.id });
+		webhook.send("cc", { avatarURL: message.author.avatarURL(), username: message.member.nickname || message.author.username}).catch(console.error);
 	}
 };
 
