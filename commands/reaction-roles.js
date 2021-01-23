1fab3c28531973390b669bfe7612971c1f2ecf87
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-23 09:47:57 +0100

    .

--- commands/reaction-roles.js
+++ commands/reaction-roles.js
@@ -1,105 +1,127 @@
 const { Message } = require("discord.js");
 
 const command = {
-    name: "reaction-roles",
-    description: "Gérer les rôles avec réaction",
-    aliases: ["rroles"],
-    args: 1,
-    usage: "create <titre> | add <ID message> <rôle> <emoji> | remove <ID message> <rôle>",
-    perms: ["MANAGE_ROLES"],
-    slashOptions: [
-        {
-            name: "create",
-            description: "Créer un nouveau message vierge",
-            type: 1,
-            options: [
-                {
-                    name: "titre",
-                    description: "Le titre du message",
-                    type: 3,
-                    required: true
-                }
-            ]
-        },
-        {
-            name: "add",
-            description: "Ajouter un nouveau rôle",
-            type: 1,
-            options: [
-                {
-                    name: "messageID",
-                    description: "L'ID du message auquel ajouter le rôle",
-                    type: 3,
-                    required: true
-                },
-                {
-                    name: "rôle",
-                    description: "Le rôle à ajouter",
-                    type: 8,
-                    required: true
-                },
-                {
-                    name: "emoji",
-                    description: "L'emoji à ajouter avec le rôle",
-                    type: 3,
-                    required: true
-                }
-            ]
-        },
-        {
-            name: "remove",
-            description: "Retirer un rôle",
-            type: 1,
-            options: [
-                {
-                    name: "messageID",
-                    description: "L'ID du message duquel retirer le rôle",
-                    type: 3,
-                    required: true
-                },
-                {
-                    name: "rôle",
-                    description: "Le rôle à retirer",
-                    type: 8,
-                    required: true
-                }
-            ]
-        }
-    ],
-    /**
+	name: "reaction-roles",
+	description: "Gérer les rôles avec réaction",
+	aliases: ["rroles"],
+	args: 2,
+	usage: "create <titre> | add <ID message> <rôle> <emoji> | remove <ID message> <rôle>",
+	perms: ["MANAGE_ROLES"],
+	slashOptions: [
+		{
+			name: "create",
+			description: "Créer un nouveau message vierge",
+			type: 1,
+			options: [
+				{
+					name: "titre",
+					description: "Le titre du message",
+					type: 3,
+					required: true
+				}
+			]
+		},
+		{
+			name: "add",
+			description: "Ajouter un nouveau rôle",
+			type: 1,
+			options: [
+				{
+					name: "messageID",
+					description: "L'ID du message auquel ajouter le rôle",
+					type: 3,
+					required: true
+				},
+				{
+					name: "rôle",
+					description: "Le rôle à ajouter",
+					type: 8,
+					required: true
+				},
+				{
+					name: "emoji",
+					description: "L'emoji à ajouter avec le rôle",
+					type: 3,
+					required: true
+				}
+			]
+		},
+		{
+			name: "remove",
+			description: "Retirer un rôle",
+			type: 1,
+			options: [
+				{
+					name: "messageID",
+					description: "L'ID du message duquel retirer le rôle",
+					type: 3,
+					required: true
+				},
+				{
+					name: "rôle",
+					description: "Le rôle à retirer",
+					type: 8,
+					required: true
+				}
+			]
+		}
+	],
+	/**
 	 * @param {Message} message 
 	 * @param {string[]} args 
 	 * @param {Object[]} options
 	 */
 	execute: async (message, args, options) => {
-        const rolesChannel = message.guild.channels.cache.get("802144513639972864");
-        const subCommand = args
-            ? args[0].toLowerCase()
-            : options[0].name;
-        
-            switch (subCommand) {
-                case "create":
-                    const title = args
-                        ? args.slice(1).join(" ")
-                        : options[0].options[0].value;
+		const rolesChannel = message.guild.channels.cache.get("802144513639972864");
+		const subCommand = args
+			? args[0].toLowerCase()
+			: options[0].name;
+		
+			switch (subCommand) {
+				case "create": {
+					const title = args
+						? args.slice(1).join(" ")
+						: options[0].options[0].value;
 
-                    const msg = await rolesChannel.send({
-                        embed: {
-                            author: {
-                                name: title,
-                                icon_url: message.client.user.avatarURL()
-                            },
-                            color: "#010101",
-                            footer: {
-                                text: "✨ Mayze ✨"
-                            }
-                        }
-                    }).catch(console.error);
-                    if (msg) message.channel.reply(`message créé (ID: ${msg.id})`).catch(console.error);
-                    else message.channel.send("Quelque chose s'est mal passé en envoyant le message :/").catch(console.error);
-                    break;
-            }
-    }
+					const msg = await rolesChannel.send({
+						embed: {
+							author: {
+								name: title,
+								icon_url: message.client.user.avatarURL()
+							},
+							color: "#010101",
+							footer: {
+								text: "✨ Mayze ✨"
+							}
+						}
+					}).catch(console.error);
+					if (msg) message.reply(`message créé (ID: ${msg.id})`).catch(console.error);
+					else message.channel.send("Quelque chose s'est mal passé en envoyant le message :/").catch(console.error);
+					break;
+				}
+				case "add": {
+					const msgID = args
+						? args[1]
+						: options[0].options[0].value;
+					const msg = await rolesChannel.messages.fetch(msgID);
+					if (!msg) return message.reply("l'ID du message est incorrect").catch(console.error);
+					const role = args
+						? message.guild.roles.cache.find(r => r.id === args[2] || r.name.toLowerCase() === args[2] || r.name.toLowerCase().includes(args[2]))
+						: message.guild.roles.cache.get(options[0].options[1].value);
+					if (!role) return message.reply("ce rôle n'existe pas").catch(console.error);
+					const emoji = args
+						? args[3]
+						: options[0].options[2].value;
+					if (!emoji) return message.reply("ajoute un emoji").catch(console.error);
+
+					const m = await msg.edit(msg.embeds[0].setDescription(`${msg.embeds[0].description}\n${emoji} • ${role}`)).catch(console.error);
+					if (!m) return message.channel.send("Quelque chose s'est mal passé en modifiant le message :/").catch(console.error);
+					if (message.deletable) message.react("✅").catch(console.error);
+					else message.reply("rôle ajouté").catch(console.error);
+					break;
+				}
+			}
+	}
 };
 
 module.exports = command;
---
+++ reaction_commands/reaction_roles.js
@@ -0,0 +1,25 @@
+const { MessageReaction, User } = require("discord.js");
+
+const command = {
+    /**
+     * @param {MessageReaction} reaction 
+     * @param {User} user 
+     * @param {boolean} added
+     */
+    execute: async (reaction, user, added) => {
+        if (reaction.message.channel.id !== "802144513639972864") return;
+        if (!reaction.message.embeds.length) return;
+        const embed = reaction.message.embeds[0];
+        const roles = embed.description.split("\n").map(e => {
+            return { emoji: e.slice(0, 1), role: reaction.message.guild.roles.cache.get(e.match(/\d{18}/)[0]) };
+        });
+
+        const role = roles.find(r => r.emoji === reaction.emoji.name);
+        if (!role) return;
+
+        if (added) reaction.message.guild.member(user).roles.add(role.role);
+        if (!added) reaction.message.guild.member(user).roles.remove(role.role);
+    }
+};
+
+module.exports = command
