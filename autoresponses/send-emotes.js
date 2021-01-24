d5d80dc0fadde9ada9a4086ef72cef06999eb564
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-24 09:33:48 +0100

    .

--- autoresponses/send-emotes.js
+++ autoresponses/send-emotes.js
@@ -10,10 +10,10 @@ const command = {
 
 		const regex = /(?:^|\s):[\w-_]+:(?:\s|$)/g;
 		if (!regex.test(message.content)) return;
-		const emojiNames = message.content.match(regex).map(e => e.replace(/\s?:\s?/g, ""));
+		const emojiNames = message.content.match(regex).map(e => e.replace(/:/g, ""));
 		if (!emojiNames.every(emojiName => message.guild.emojis.cache.find(emoji => emoji.name === emojiName))) return;
 
-		const newMsg = message.content.replace(regex, a => message.client.emojis.cache.find(e => e.name === a.replace(/:/g, "")));
+		const newMsg = message.content.replace(regex, a => ` ${message.client.emojis.cache.find(e => e.name === a.replace(/:/g, ""))} `);
 
 		const webhook = await message.client.fetchWebhook(webhookID).catch(console.error);
 		if (!webhook) return;
