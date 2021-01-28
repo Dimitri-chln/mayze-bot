e15d07ed4b1c6d841aa4ba420efe23d181260d67
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-28 10:39:01 +0100

    Playlist support!

--- music_commands/play.js
+++ music_commands/play.js
@@ -15,24 +15,31 @@ const command = {
 	 */
 	execute: async (message, args, options) => {
 		if (!message.member.voice.channelID || (message.client.player.getQueue(message.guild.id) && message.member.voice.channelID !== message.client.player.getQueue(message.guild.id).connection.channel.id)) return message.reply("tu n'es pas dans le même salon vocal que moi").catch(console.error);
+		const isPlaying = message.client.player.isPlaying(message.guild.id);
 
 		const playlistRegex = /^((?:https?:)\/\/)?((?:www|m)\.)?((?:youtube\.com)).*(youtu.be\/|list=)([^#\&\?]*).*/;
 		const search = args
 			? args.join(" ")
 			: options[0].value;
-		if (playlistRegex.test(search)) return message.reply("les playlists ne sont pas supportées pour le moment").catch(console.error);
 		
-		const isPlaying = message.client.player.isPlaying(message.guild.id);
-        // If there's already a song playing
-        if (isPlaying) {
-            // Add the song to the queue
-            const { song } = await message.client.player.addToQueue(message.guild.id, search, null, message.author);
-            message.channel.send(`<a:blackCheck:803603780666523699> | **Ajouté à la queue**\n> ${song.name}`).catch(console.error);
-        } else {
-            // Else, play the song
-            const { song } = await message.client.player.play(message.member.voice.channel, search, null, message.author);
-            message.channel.send(`<a:blackCheck:803603780666523699> | **En train de jouer...**\n> ${song.name}`).catch(console.error);
-        }
+		if (playlistRegex.test(search)) {
+			const playlist = await message.client.player.playlist(message.guild.id, playlistLink, message.member.voice.channel, -1, message.author);
+			message.channel.send(`<a:blackCheck:803603780666523699> | **Playlist ajoutée**\n> ${playlist.playlist.videoCount} musiques ont été ajoutées à la queue`).catch(console.error);
+			if (!isPlaying) message.channel.send(`<a:blackCheck:803603780666523699> | **En train de jouer...**\n> ${playlist.song.name}`).catch(console.error);
+
+		} else {
+		
+			// If there's already a song playing
+			if (isPlaying) {
+				// Add the song to the queue
+				const { song } = await message.client.player.addToQueue(message.guild.id, search, null, message.author);
+				message.channel.send(`<a:blackCheck:803603780666523699> | **Ajouté à la queue**\n> ${song.name}`).catch(console.error);
+			} else {
+				// Else, play the song
+				const { song } = await message.client.player.play(message.member.voice.channel, search, null, message.author);
+				message.channel.send(`<a:blackCheck:803603780666523699> | **En train de jouer...**\n> ${song.name}`).catch(console.error);
+			}
+		}
 	}
 };
 
