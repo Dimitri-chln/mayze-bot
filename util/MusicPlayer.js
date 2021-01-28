206290e1480a51b4b5694960a1ce961f52bc6910
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-28 09:00:20 +0100

    .

--- util/MusicPlayer.js
+++ util/MusicPlayer.js
@@ -469,7 +469,7 @@ class Util {
 		const progressText = loadedIcon.repeat(progress) + arrowIcon;
 		const emptyProgressText = loadedIcon.repeat(emptyProgress);
 
-		return `[[${progressText}](https://google.com)${emptyProgressText}][${this.MillisecondsToTime(value)}/${this.MillisecondsToTime(maxValue)}]`;
+		return `[${progressText}](https://google.com)${emptyProgressText} [${this.MillisecondsToTime(value)}/${this.MillisecondsToTime(maxValue)}]`;
 	};
 
 
