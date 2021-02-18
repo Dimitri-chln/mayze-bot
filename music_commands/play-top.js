6c4bc5a7453b332f6568d70bb0f20f9ea0fe0159
Dimitri-chln <dimitrichaline4@gmail.com>
2021-02-18 12:10:28 +0100

    .

--- music_commands/play-top.js
+++ music_commands/play-top.js
@@ -24,7 +24,7 @@ const command = {
 		if (playlistRegex.test(search)) return message.reply("les playlists ne sont pas supportées pour cette commande").catch(console.error);
 		
 		const res = await message.client.player.playtop(message.guild.id, message.member.voice.channel, search, null, message.author);
-		if (res.error) return message.reply(`je n'ai pas trouvé de musique avec ce titre`).catch(console.error);
+		if (!res.song) return message.reply(`je n'ai pas trouvé de musique avec ce titre`).catch(console.error);
 		// If there's already a song playing
 		if (isPlaying) message.channel.send(`<a:blackCheck:803603780666523699> | **Ajouté en début de queue**\n> ${res.song.name}`).catch(console.error);
 		else message.channel.send(`<a:blackCheck:803603780666523699> | **En train de jouer...**\n> ${res.song.name}`).catch(console.error);
