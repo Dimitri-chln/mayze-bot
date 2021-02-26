eb56e228bc9fe1af726d587b30a106a62d9be067
Dimitri-chln <dimitrichaline4@gmail.com>
2021-02-26 16:21:34 +0100

    Get volume

--- music_commands/volume.js
+++ music_commands/volume.js
@@ -4,8 +4,8 @@ const command = {
 	name: "volume",
 	description: "Ajuster le volume de la musique",
 	aliases: ["vol"],
-	args: 1,
-	usage: "<volume>",
+	args: 0,
+	usage: "[<volume>]",
 	disableSlash: true,
 	/**
 	 * @param {Message} message 
@@ -14,17 +14,22 @@ const command = {
 	 */
 	execute: async (message, args, options, language, languageCode) => {
 		if (!message.member.voice.channelID || (message.client.player.getQueue(message.guild.id) && message.member.voice.channelID !== message.client.player.getQueue(message.guild.id).connection.channel.id)) return message.reply("tu n'es pas dans le même salon vocal que moi").catch(console.error);
-		
-		const volume = args
-			? parseInt(args[0])
-			: options[0].value;
-		if (isNaN(volume) || volume < 0 || volume > 200) return message.reply("le volume doit être compris entre 0 et 200").catch(console.error);
-
 		const isPlaying = message.client.player.isPlaying(message.guild.id);
 		if (!isPlaying) return message.channel.send("Il n'y a aucune musique en cours sur ce serveur").catch(console.error);
+		
+		if (args[0]) {
+			const volume = args
+				? parseInt(args[0])
+				: options[0].value;
+			if (isNaN(volume) || volume < 0 || volume > 200) return message.reply("le volume doit être compris entre 0 et 200").catch(console.error);
 
-		message.client.player.setVolume(message.guild.id, volume);
-		message.channel.send(`<a:blackCheck:803603780666523699> | **Volume modifié**\n> ${volume}%`).catch(console.error);
+			message.client.player.setVolume(message.guild.id, volume);
+			message.channel.send(`<a:blackCheck:803603780666523699> | **Volume modifié**\n> ${volume}%`).catch(console.error);
+		
+		} else {
+			const volume = message.client.player.getQueue(message.guild.id).dispatcher.volumeLogarithmic * 200;
+			message.channel.send(`<a:blackCheck:803603780666523699> | **Volume: ${volume}%**`).catch(console.error);
+		}
 	}
 };
 
