559495515aaa96f8653fa1388568fa2875a3f906
Dimitri-chln <dimitrichaline4@gmail.com>
2021-03-09 20:45:00 +0100

    .

--- utils/EnhancedInteraction.js
+++ utils/EnhancedInteraction.js
@@ -3,61 +3,51 @@ const Axios = require("axios").default;
 
 class EnhancedInteraction {
 	/**@type {string} */
-	#id;
+	id;
 	/**@type {string} */
-	#token;
+	token;
 	/**@type {Object} */
-	#base;
+	base;
 	/**@type {Client} */
-	#client;
+	client;
 	/**@type {string} */
-	#applicationID;
+	applicationID;
 	/**@type {Guild} */
-	#guild;
+	guild;
 	/**@type {Channel} */
-	#channel;
+	channel;
 	/**@type {User} */
-	#author;
+	author;
 	/**@type {GuildMember} */
-	#member;
+	member;
 
 	/**
 	 * @param {Object} interaction 
 	 * @param {Client} client 
 	 */
 	constructor(interaction, client) {
-		this.#id = interaction.id;
-		this.#token = interaction.token;
-		this.#base = interaction;
-		this.#client = client;
-		this.#applicationID = this.#client.user.id;
-		if (interaction.guild_id) this.#guild = this.#client.guilds.cache.get(interaction.guild_id);
-		if (interaction.channel_id) this.#channel = this.#client.channels.cache.get(interaction.channel_id);
-		if (interaction.member) this.#member = this.#guild.members.cache.get(interaction.member.user.id);
-		this.#author = this.#member.user;
+		this.isInteraction = true;
+		this.id = interaction.id;
+		this.token = interaction.token;
+		this.base = interaction;
+		this.client = client;
+		this.applicationID = this.client.user.id;
+		if (interaction.guild_id) this.guild = this.client.guilds.cache.get(interaction.guild_id);
+		if (interaction.channel_id) this.channel = this.client.channels.cache.get(interaction.channel_id);
+		if (interaction.member) this.member = this.guild.members.cache.get(interaction.member.user.id);
+		this.author = this.member.user;
 	}
-
-	get isInteraction() { return true; }
-	get id() { return this.#id; }
-	get token() { return this.#token; }
-	get base() { return this.#base; }
-	get client() { return this.#client; }
-	get applicationID() { return this.#applicationID; }
-	get guild() { return this.#guild; }
-	get channel() { return this.#channel; }
-	get author() { return this.#author; }
-	get member() { return this.#member; }
 
 	/**
 	 * @param {string} content The reply message
 	 */
 	async reply(content) {
-		const message = await this.channel.send(`${this.#author}, ${content}`);
+		const message = await this.channel.send(`${this.author}, ${content}`);
 		return message;
 	}
 
 	async acknowledge() {
-		const url = `https://discord.com/api/v8/interactions/${this.#id}/${this.#token}/callback`;
+		const url = `https://discord.com/api/v8/interactions/${this.id}/${this.token}/callback`;
 
 		const res = await Axios.post(url, {
 			type: 4,
@@ -74,7 +64,7 @@ class EnhancedInteraction {
 	 * @param {string} content
 	 */
 	async respond(content) {
-		const url = `https://discord.com/api/v8/webhooks/${this.#applicationID}/${this.#id}/${this.#token}/messages/@original`;
+		const url = `https://discord.com/api/v8/webhooks/${this.applicationID}/${this.id}/${this.token}/messages/@original`;
 
 		const res = await Axios.patch(url, {
 			content,
@@ -84,7 +74,7 @@ class EnhancedInteraction {
 	}
 
 	async delete() {
-		const url = `https://discord.com/api/v8/webhooks/${this.#applicationID}/${this.#id}/${this.#token}/messages/@original`;
+		const url = `https://discord.com/api/v8/webhooks/${this.applicationID}/${this.id}/${this.token}/messages/@original`;
 
 		const res = await Axios.delete(url).catch(console.error);
 
