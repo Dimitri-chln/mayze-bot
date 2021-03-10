37e15cd182054f2752443e54d6296e238d45441c
Dimitri-chln <dimitrichaline4@gmail.com>
2021-03-10 12:35:13 +0100

    .

--- bot.js
+++ bot.js
@@ -189,11 +189,10 @@ client.ws.on("INTERACTION_CREATE", async interaction => {
 	const enhancedInteraction = new EnhancedInteraction(interaction, client);
 
 	console.log(`${enhancedInteraction.author.tag} used /${enhancedInteraction.base.data.name} in #${enhancedInteraction.channel.name}\n${JSON.stringify(enhancedInteraction.base.data, null, 4)}`);
-	await enhancedInteraction.acknowledge();
-	enhancedInteraction.respond("This is a test");
+	enhancedInteraction.acknowledge();
 	
 	if (command) await processCommand(command, enhancedInteraction, null, options);
-	setTimeout(enhancedInteraction.delete, 5000);
+	// setTimeout(enhancedInteraction.delete, 5000);
 });
 
 /**
