4e0fc19a8b2955335146ef24d5edcfb0c20c9b65
Dimitri-chln <dimitrichaline4@gmail.com>
2021-03-25 09:12:59 +0100

    fix²

--- bot.js
+++ bot.js
@@ -147,7 +147,7 @@ client.on("ready", async () => {
 		reminders.forEach(reminder => {
 			const timestamp = new Date(reminder.timestamp).valueOf();
 			if (timestamp < Date.now()) {
-				client.users.fetch(reminder.user_id).then(user => user.send(`⏰ | ${reminder.content}`).catch(console.error)).catch(console.error);
+				client.users.fetch(reminder.user_id).then(user => user.send(`⏰ | ${reminder.content.replace(/U\+0027/g, "'")}`).catch(console.error)).catch(console.error);
 				client.pg.query(`DELETE FROM reminders WHERE id = ${reminder.id}`).catch(console.error);
 			}
 		});
