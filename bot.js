6932ee650dde909e01309423a9e2655b729063c9
Dimitri-chln <dimitrichaline4@gmail.com>
2021-03-27 13:59:16 +0100

    .

--- bot.js
+++ bot.js
@@ -486,9 +486,9 @@ function slashChanged(oldSlash, newSlash) {
  * @param {Discord.GuildMember} member The member to check
  */
 function checkUnpingable(member) {
-	const regex = new RegExp(`[\\w\\déèêëàâôîùûç]{${member.displayName.length < 3 ? member.displayName.length : 3}}`, "i");
+	const regex = new RegExp(`[\\w\\déèêëàâôîùûç"'\\(\\)\\[\\]\\{\\}&#-_\\^@°=\\+\\$\\*%!:\\/;\\.,\\?\\\\<>]{${member.displayName.length < 3 ? member.displayName.length : 3}}`, "i");
 	if (member.guild.id !== "689164798264606784") return;
-	if (!regex.test(member.displayName)) member.setNickname(member.displayName + "\u2063", "Unpingable nickname or username").catch(console.error);
+	if (!regex.test(member.displayName)) member.setNickname(`.${member.displayName}`, "Unpingable nickname or username").catch(console.error);
 }
 
 
