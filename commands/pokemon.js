0e0a9af5602f76616526b10fdd001a0306fdafee
Dimitri-chln <dimitrichaline4@gmail.com>
2021-04-05 12:16:30 +0200

    Allow users to check other's pokemons

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -8,8 +8,14 @@ const command = {
 	},
 	aliases: ["pokemons", "pkmn", "pkm", "poke"],
 	args: 0,
-	usage: "[-legendary] [-shiny] [-id [<number>]] [-name <name>]",
+	usage: "[<user>] [-legendary] [-shiny] [-id [<number>]] [-name <name>]",
 	slashOptions: [
+		{
+			name: "user",
+			description: "The user to check",
+			type: 6,
+			required: false
+		},
 		{
 			name: "options",
 			description: "Search options",
@@ -27,12 +33,16 @@ const command = {
 		const pokedex = require("oakdex-pokedex");
 		const pagination = require("../utils/pagination");
 
-		let { "rows": pokemons }  = (await message.client.pg.query(`SELECT * FROM pokemons WHERE user_id = '${message.author.id}' ORDER BY legendary DESC, shiny DESC, caught DESC, pokedex_id ASC`).catch(console.error)) || {};
+		const user = args
+			? message.mentions.users.first()
+			: options.some(o => o.name === "user") ? message.guild.members.cache.get(options.find(o => o.name === "user").value).user : message.author;
+
+		let { "rows": pokemons }  = (await message.client.pg.query(`SELECT * FROM pokemons WHERE user_id = '${user.id}' ORDER BY legendary DESC, shiny DESC, caught DESC, pokedex_id ASC`).catch(console.error)) || {};
 		if (!pokemons) return message.channel.send(language.errors.database).catch(console.error);
 
 		const params = args
 			? args
-			: (options ? options[0] : { value: "" }).value.split(" ");
+			: options.some(o => o.name === "options") ? options.find(o => o.name === "options").value.split(/ +/) : [];
 
 		if (params.includes("-legendary")) pokemons = pokemons.filter(p => p.legendary);
 		if (params.includes("-shiny")) pokemons = pokemons.filter(p => p.shiny);
@@ -42,14 +52,14 @@ const command = {
 		const pkmPerPage = 15;
 		let pages = [];
 		let embed = new MessageEmbed()
-			.setAuthor(language.get(language.title, message.author.tag), message.author.avatarURL({ dynamic: true }))
+			.setAuthor(language.get(language.title, user.tag), user.avatarURL({ dynamic: true }))
 			.setColor(message.guild.me.displayHexColor)
 			.setDescription(language.no_pokemon);
 		if (!pokemons.length) pages.push(embed);
 
 		for (i = 0; i < pokemons.length; i += pkmPerPage) {
 			embed = new MessageEmbed()
-				.setAuthor(language.get(language.title, message.author.tag), message.author.avatarURL({ dynamic: true }))
+				.setAuthor(language.get(language.title, user.tag), user.avatarURL({ dynamic: true }))
 				.setColor(message.guild.me.displayHexColor)
 				.setDescription(pokemons.slice(i, i + pkmPerPage).map(p => language.get(language.description, p.legendary ? "🎖️ " : "", p.shiny ? "⭐ " : "", pokedex.findPokemon(p.pokedex_id).names[languageCode], params.includes("-id") ? `#${p.pokedex_id}` : "", p.caught, p.caught > 1 ? "s" : "")).join("\n"));
 			pages.push(embed);
