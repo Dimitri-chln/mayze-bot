663ddf414938a6e115942cf8fad761d5c6fef025
Dimitri-chln <dimitrichaline4@gmail.com>
2021-04-07 19:31:17 +0200

    .

---
+++ responses/dim.js
@@ -0,0 +1,35 @@
+const { Message } = require("discord.js");
+
+const command = {
+	/**
+	 * @param {Message} message 
+	 */
+	execute: async message => {
+		const dim = await message.client.users.fetch("307815349699608577").catch(console.error);
+
+		const regex = /\bdimi?(?:tri)?\b/i;
+		
+		if (regex.test(message.content)) {
+			dim.send({
+				embed: {
+					author: {
+						name: message.author.tag,
+						icon_url: message.author.avatarURL({ dynamic: true }),
+						url: message.url
+					},
+					title: `#${message.channel.name}`,
+					color: "#010101",
+					description: message.content,
+					image: {
+						url: message.attachments.size ? message.attachments.first().url : null
+					},
+					footer: {
+						text: "✨ Mayze ✨"
+					}
+				}
+			}).catch(console.error);
+		}
+	}
+};
+
+module.exports = command;
