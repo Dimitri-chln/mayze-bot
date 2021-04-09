833fe482d54c41f6883cbff7feffd2b2c9a2fb98
Dimitri-chln <dimitrichaline4@gmail.com>
2021-04-09 14:54:22 +0200

    .

--- commands/discord-images.js
+++ commands/discord-images.js
@@ -7,27 +7,30 @@ const command = {
 	args: 0,
 	usage: "",
 	ownerOnly: true,
-    /**
+	/**
 	 * @param {Message} message 
 	 * @param {string[]} args 
 	 * @param {Object[]} options
 	 */
 	execute: async (message, args, options, language, languageCode) => {
-        const Fs = require("fs");
+		const Fs = require("fs");
 
-        if (message.channel.id !== "744291145504522252") return;
+		if (message.channel.id !== "744291145504522252") return;
 
-        Fs.readdir("../discord-images", async (err, files) => {
-            if (err) return message.reply("An error occured").catch(console.error);
+		Fs.readdir("./discord-images", async (err, files) => {
+			if (err) {
+				console.error(err);
+				return message.channel.send("An error occured").catch(console.error);
+			}
 
-            for (const file of files.filter(f => !f.endsWith(".txt"))) {
-                Fs.readFile(`../discord-images/${file}`, async (err, buffer) => {
-                    if (err) return message.reply(`Failed loading ${file}`).catch(console.error);
-                    message.channel.send({ files: [ buffer ] }).catch(console.error);
-                });
-            }
-        });
-    }
+			for (const file of files.filter(f => !f.endsWith(".txt"))) {
+				Fs.readFile(`./discord-images/${file}`, async (err, buffer) => {
+					if (err) return message.reply(`Failed loading ${file}`).catch(console.error);
+					message.channel.send({ files: [ buffer ] }).catch(console.error);
+				});
+			}
+		});
+	}
 };
 
 module.exports = command;
