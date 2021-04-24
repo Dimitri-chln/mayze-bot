356749c706d02608ae8622efd9c43f726ac85afd
Dimitri-chln <dimitrichaline4@gmail.com>
2021-04-24 11:46:08 +0200

    .

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -95,7 +95,9 @@ const command = {
 		const timeToString = require("../utils/timeToString");
 		const updateGwaMsg = require("../utils/updateGwaMsg");
 		const { GIVEAWAY_CHANNEL_ID } = require("../config.json");
-		const channel = message.client.channels.cache.get(GIVEAWAY_CHANNEL_ID);
+		const channel = message.channel.id === GIVEAWAY_CHANNEL_ID
+			? message.channel
+			: message.client.channels.cache.get(GIVEAWAY_CHANNEL_ID);
 
 		if (!message.member.hasPermission("MANAGE_MESSAGES") && !message.member.roles.has("815884135410958336"))
 			return message.reply("tu n'as pas la permission de créer des giveaways").catch(console.error);
@@ -116,7 +118,7 @@ const command = {
 				if (!duration) return message.reply("la durée est invalide (dhms)").catch(console.error);
 				const endTimestamp = new Date(Date.now() + duration);
 				const winners = args
-					? args[3] ? (isNaN(parseInt(args[3].replace("w", ""))) ? 1 : parseInt(args[3].replace("w", ""))) : 1
+					? args[3] ? (isNaN(parseInt(args[3].replace(/^(\d+)w?/, "$1"))) ? 1 : parseInt(args[3].replace(/^(\d+)w?/, "$1", ""))) : 1
 					: options[0].options.some(o => o.name === "gagnants") ? options[0].options.find(o => o.name === "gagnants").value : 1;
 				if (winners < 1) return message.reply("le nombre de gagnants doit être supérieur à 1");
 				const mention = args
