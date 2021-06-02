97fa8deab0f62e31ae1aa231dbb58baf9e10e51c
Dimitri-chln <dimitrichaline4@gmail.com>
2021-06-02 14:45:04 +0200

    .

---
+++ responses/mila.js
@@ -0,0 +1,35 @@
+const { Message } = require("discord.js");
+
+const command = {
+	/**
+	 * @param {Message} message 
+	 */
+	execute: async message => {
+		const mila = await message.client.users.fetch("608623753399762964").catch(console.error);
+
+		const regex = /\bmila(?:y(?=na))(?:na)?\b/i;
+		
+		if (message.author.id !== mila.id && regex.test(message.content) && !message.mentions.users.has(mila.id)) {
+			mila.send({
+				embed: {
+					author: {
+						name: message.author.tag,
+						icon_url: message.author.avatarURL({ dynamic: true }),
+						url: message.url
+					},
+					title: `#${message.channel.name}`,
+					color: 65793,
+					description: message.content,
+					image: {
+						url: message.attachments.size ? message.attachments.first().url : null
+					},
+					footer: {
+						text: "✨ Mayze ✨"
+					}
+				}
+			}).catch(console.error);
+		}
+	}
+};
+
+module.exports = command;
