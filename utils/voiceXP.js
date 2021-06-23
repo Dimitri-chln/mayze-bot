81fb466438e085ae3680bd1d8c2b77e21d8c95c7
Dimitri-chln <dimitrichaline4@gmail.com>
2021-06-23 22:58:30 +0200

    .

--- utils/voiceXP.js
+++ utils/voiceXP.js
@@ -16,7 +16,7 @@ async function voiceXP(member, xp, languageCode = "en") {
 	const getLevel = require("./getLevel");
 	
 	const { rows } = (await member.client.pg.query(
-		"INSERT INTO levels (user_id, voice_xp) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET voice_xp = voice_xp + $2 WHERE levels.user_id = $1 RETURNING voice_xp",
+		"INSERT INTO levels (user_id, voice_xp) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET voice_xp = voice_xp + $2 WHERE levels.user_id = $1 RETURNING levels.voice_xp",
 		[ member.user.id, xp ]
 	).catch(console.error)) || {};
 
