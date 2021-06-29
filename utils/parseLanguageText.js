7c705eff3b074b4dc9aaea32e4c2687c6930a709
Dimitri-chln <dimitrichaline4@gmail.com>
2021-06-29 12:53:18 +0200

    .

--- utils/parseLanguageText.js
+++ utils/parseLanguageText.js
@@ -5,27 +5,35 @@
  * @returns {string}
  */
 function parse(text, ...args) {
-    text = text
-        .replace(/\{\d+?\}/g, a => args[parseInt(a.replace(/[\{\}]/g, "")) - 1]);
+	text = text
+		.replace(/{/g, "~c")
+		.replace(/}/g, "~b")
+		.replace(/\[/g, "~s")
+		.replace(/\]/g, "~t")
+		.replace(/:/g, "~d")
+		.replace(/\?/g, "~q");
 
-    while (/\[\d+?\?[^\[\]]*?:.*?\]/gs.test(text)) {
-        text = text
-            .replace(/\[\d+?\?[^\[\]]*?:.*?\]/gs, a => {
-                let m = a.match(/\[(\d+?)\?([^\[\]]*?):(.*?)\]/s);
-                if (args[parseInt(m[1]) - 1]) return m[2];
-                else return m[3];
-            });
-    }
+	text = text
+		.replace(/\{\d+?\}/g, a => args[parseInt(a.replace(/[\{\}]/g, "")) - 1]);
 
-    text = text
-        .replace(/~c/g, "{")
-        .replace(/~b/g, "}")
-        .replace(/~s/g, "[")
-        .replace(/~t/g, "]")
-        .replace(/~d/g, ":")
-        .replace(/~q/g, "?");
+	while (/\[\d+?\?[^\[\]]*?:.*?\]/gs.test(text)) {
+		text = text
+			.replace(/\[\d+?\?[^\[\]]*?:.*?\]/gs, a => {
+				let m = a.match(/\[(\d+?)\?([^\[\]]*?):(.*?)\]/s);
+				if (args[parseInt(m[1]) - 1]) return m[2];
+				else return m[3];
+			});
+	}
 
-    return text;
+	text = text
+		.replace(/~c/g, "{")
+		.replace(/~b/g, "}")
+		.replace(/~s/g, "[")
+		.replace(/~t/g, "]")
+		.replace(/~d/g, ":")
+		.replace(/~q/g, "?");
+
+	return text;
 }
 
 module.exports = parse;
