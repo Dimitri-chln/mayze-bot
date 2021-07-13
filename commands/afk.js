a216bc1f86d1b66bb17753a93d68b719b45dbef8
Dimitri-chln <dimitrichaline4@gmail.com>
2021-07-13 20:31:48 +0200

    fixed afk

--- commands/afk.js
+++ commands/afk.js
@@ -28,12 +28,17 @@ const command = {
 			? args.join(" ").replace(/^./, a => a.toUpperCase())
 			: options ? options[0].value.replace(/^./, a => a.toUpperCase()) : "";
 		
-		const { rows } = (await message.client.pg.query(`SELECT * FROM afk WHERE user_id = '${message.author.id}'`).catch(console.error)) || {};
-		if (!rows) return message.channel.send(language.errors.database, { ephemeral: true }).catch(console.error);
+		const res = await message.client.pg.query(
+			`
+			INSERT INTO afk VALUES ($1, $2)
+			ON CONFLICT (user_id)
+			DO UPDATE SET message = $2
+			WHERE afk.user_id = EXLUDED.user_id
+			`,
+			[ message.author.id, AKFmessage ]
+		).catch(console.error);
 
-		if (rows.length) message.client.pg.query(`DELETE FROM afk WHERE user_id = '${message.author.id}'`).catch(console.error);
-		if (AKFmessage) message.client.pg.query(`INSERT INTO afk (user_id, message) VALUES ('${message.author.id}', '${AKFmessage}')`).catch(console.error);
-		else message.client.pg.query(`INSERT INTO afk (user_id) VALUES ('${message.author.id}')`).catch(console.error);
+		if (!res) return message.channel.send(language.errors.database, { ephemeral: true }).catch(console.error);
 
 		message.channel.send(language.get(language.afk_message, message.author.toString(), AKFmessage ? `\n**→ ${AKFmessage}**` : ""), { disableMentions: "everyone", ephemeral: true }).catch(console.error);
 	}
