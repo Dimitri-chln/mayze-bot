543bff7673fc6bc8d27a0ef07863825dd2837c11
Dimitri-chln <dimitrichaline4@gmail.com>
2021-07-19 13:38:47 +0200

    .

--- bot.js
+++ bot.js
@@ -516,10 +516,19 @@ client.on("guildMemberAdd", async member => {
 });
 
 client.on("guildMemberUpdate", async (oldMember, member) => {
-	checkUnpingable(member);
+	if (oldMember.roles.cache.every(r => member.roles.cache.has(r.id)) && member.roles.cache.every(r => oldMember.roles.cache.has(r.id))) {
+		client.pg.query(
+			`
+			INSERT INTO member_roles VALUES ($1, $2)
+			ON CONFLICT (user_id)
+			DO UPDATE SET roles = $2
+			WHERE member_roles.user_id = EXLUDED.user_id
+			`,
+			[ member.user.id, Array.from(member.roles.cache).map(r => r[0]) ]
+		).catch(console.error);
+	}
 
-	if (!oldMember.roles.cache.every(r => member.roles.cache.has(r.id))) return;
-	if (!member.roles.cache.every(r => oldMember.roles.cache.has(r.id))) return;
+	checkUnpingable(member);
 });
 
 client.on("guildMemberRemove", async member => {
