a15c0f2fe4794b210aa00d64ad8fe93952af01ad
Dimitri-chln <dimitrichaline4@gmail.com>
2021-07-21 23:02:47 +0200

    .

--- bot.js
+++ bot.js
@@ -349,6 +349,9 @@ client.ws.on("INTERACTION_CREATE", async interaction => {
 			break;
 		case 2: {
 			// Slash Command
+			const channelCooldown = client.channelCooldowns.get(message.channel.id);
+			if (channelCooldown) return;
+
 			const command = client.commands.get(interaction.data.name) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(interaction.data.name));
 			const options = interaction.data.options;
 			const SlashCommand = require("./utils/interactions/SlashCommand");
