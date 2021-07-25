ef876f71114de6d1d8d6ac8cf714c92462c31056
Dimitri-chln <dimitrichaline4@gmail.com>
2021-07-25 13:10:25 +0200

    .

--- utils/espapeMarkdown.js
+++ utils/espapeMarkdown.js
@@ -3,7 +3,7 @@
  * @returns {string}
  */
 function escapeMarkdown(string) {
-	return string 
+	return string
 		? string
 			.replace(/^>|\|\||[*_`]/gm, "\\$&")
 			.replace(/(\[.*\])(\(https?:\/\/.+\))/gs, "$1\\$2")
