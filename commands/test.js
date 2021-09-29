e0b44ec10ec6263f73abffecf793d2bacfee386c
Dimitri-chln <dimitrichaline4@gmail.com>
2021-09-29 10:49:55 +0200

    .

--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,4 @@
-const { Message } = require("discord.js");
+const { Message, Collection } = require("discord.js");
 
 const command = {
 	name: "test",
@@ -6,7 +6,7 @@ const command = {
 	aliases: [],
 	args: 0,
 	usage: "",
-	// ownerOnly: true,
+	ownerOnly: true,
 	/**
 	 * @param {Message} message 
 	 * @param {string[]} args 
@@ -17,22 +17,33 @@ const command = {
 		const { CATCH_REWARD, SHINY_MULTIPLIER, ALOLAN_MULTIPLIER } = require("../config.json");
 
 		const { "rows": pokemons }  = (await message.client.pg.query(
-			"SELECT * FROM pokemons WHERE users ? $1 ORDER BY legendary DESC, ultra_beast DESC, shiny DESC, (users -> $1 -> 'caught')::int DESC, pokedex_id ASC",
-			[ message.author.id ]
+			"SELECT * FROM pokemons"
 		).catch(console.error)) || {};
 		if (!pokemons) return message.channel.send(language.errors.database).catch(console.error);
 
-		let money = 0;
+		const users = new Collection();
 
 		for (const pokemon of pokemons) {
-			money += Math.round(CATCH_REWARD * (
+			const catchReward = Math.round(CATCH_REWARD * (
 				255 / (
 					pokemon.legendary || pokemon.ultra_beast ? 3 : (pokedex.findPokemon(pokemon.pokedex_id) ?? pokedex.findPokemon("Snover")).catch_rate
 				) * (pokemon.shiny ? SHINY_MULTIPLIER : 1) * (pokemon.variation === "alolan" ? ALOLAN_MULTIPLIER : 1)
 			));
+
+			for (const userID of Object.keys(pokemon.users)) {
+				users.set(
+					userID,
+					(users.get(userID) ?? 0) + catchReward * pokemon.users[userID].caught
+				);
+			}
 		}
 
-		message.channel.send(`**✨${money}**`).catch(console.error);
+		message.channel.send({
+			embed: {
+				color: message.guild.me.displayColor,
+				description: users.map((money, userID) => `<@${userID}> => ${money}`).join("\n")
+			}
+		}).catch(console.error);
 	}
 };
 
