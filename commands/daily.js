acf883ec73222d0c49fcc29760e7d12dbadb8d52
Dimitri-chln <dimitrichaline4@gmail.com>
2021-10-10 18:20:19 +0200

    Update daily cooldown

--- commands/daily.js
+++ commands/daily.js
@@ -17,7 +17,10 @@ const command = {
 	*/
 	execute: async (message, args, options, language, languageCode) => {
 		const { DAILY_REWARD }= require("../config.json");
-		const DAY_IN_MS = 1000 * 60 * 60 * 24, NOW = Date.now();
+		const DAY_IN_MS = 1000 * 60 * 60 * 24;
+		const NOW = new Date();
+		const MIDNIGHT = new Date();
+			MIDNIGHT.setHours(0, 0, 0, 0);
 
 		const { rows } = (await message.client.pg.query(
 			"SELECT * FROM currency WHERE user_id = $1",
@@ -25,14 +28,12 @@ const command = {
 		).catch(console.error)) || {};
 		if (!rows) return message.channel.send(language.errors.database).catch(console.error);
 
-		const { "last_daily": lastDaily } = rows.length
-			? rows[0]
-			: { lastDaily: null };
+		const lastDaily = rows.length
+			? new Date(rows[0].last_daily)
+			: new Date(0);
 		
-		const nextDaily = lastDaily ? Date.parse(lastDaily) + DAY_IN_MS : NOW;
-
-		if (nextDaily > NOW) {
-			const timeLeft = Math.ceil((nextDaily - NOW) / 1000);
+		if (lastDaily.valueOf() > MIDNIGHT.valueOf()) {
+			const timeLeft = Math.ceil((MIDNIGHT.valueOf() + DAY_IN_MS.valueOf() - NOW.valueOf()) / 1000);
 			const timeLeftHumanized = new Date((timeLeft % 86400) * 1000)
 				.toUTCString()
 				.replace(/.*(\d{2}):(\d{2}):(\d{2}).*/, "$1h $2m $3s")
