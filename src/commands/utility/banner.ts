88710e4ec2b7b90bef9f273d32ce8d39bf5c0f93
Dimitri-chln <dimitrichaline4@gmail.com>
2022-02-10 18:51:38 +0100

    Added /banner

---
+++ src/commands/utility/banner.ts
@@ -0,0 +1,59 @@
+import { Message } from "discord.js";
+import Command from "../../types/structures/Command";
+import Util from "../../Util";
+
+const command: Command = {
+	name: "banner",
+	description: {
+		fr: "Obtenir la bannière de profil d'un utilisateur",
+		en: "Get a user's profile banner",
+	},
+	userPermissions: [],
+	botPermissions: ["EMBED_LINKS"],
+
+	options: {
+		fr: [
+			{
+				name: "user",
+				description: "L'utilisateur dont tu veux voir la bannière de profil",
+				type: "USER",
+				required: false,
+			},
+		],
+		en: [
+			{
+				name: "user",
+				description: "The user whose profile banner you want to see",
+				type: "USER",
+				required: false,
+			},
+		],
+	},
+
+	run: async (interaction, translations) => {
+		const user = interaction.options.getUser("user") ?? interaction.user;
+
+		interaction.followUp({
+			embeds: [
+				{
+					author: {
+						name: translations.strings.title(user.tag),
+						iconURL: interaction.client.user.displayAvatarURL(),
+					},
+					color: interaction.guild.me.displayColor,
+					image: {
+						url: user.bannerURL({
+							size: 4096,
+							dynamic: true,
+						}),
+					},
+					footer: {
+						text: "✨ Mayze ✨",
+					},
+				},
+			],
+		});
+	},
+};
+
+export default command;
