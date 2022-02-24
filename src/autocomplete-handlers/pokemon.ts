c3b1a266f3a461123a9b3a61ebd8dd7fd9e2f755
Dimitri-chln <dimitrichaline4@gmail.com>
2022-02-24 18:20:59 +0100

    Add: Autocomplete

--- src/autocomplete-handlers/pokemon.ts
+++ src/autocomplete-handlers/pokemon.ts
@@ -134,6 +134,25 @@ const autocompleteHandler: AutocompleteHandler = {
 				});
 			},
 		},
+		{
+			subCommandGroup: null,
+			subCommand: "list",
+			name: "evolution",
+			type: "STRING",
+			filterType: "CONTAINS",
+			run: async (interaction, value) => {
+				const language = Util.guildConfigs.get(interaction.guild.id).language;
+
+				const pokemons = Util.pokedex.pokemons;
+
+				return pokemons.map((pokemon) => {
+					return {
+						name: pokemon.formatName(language, false, "default"),
+						value: pokemon.formatName(language, false, "default", "raw"),
+					};
+				});
+			},
+		},
 	],
 };
 
