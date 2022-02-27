89f206446e61f60a63372bf2eb5ce9c6d2a42853
Dimitri-chln <dimitrichaline4@gmail.com>
2022-02-27 20:46:41 +0100

    .

---
+++ src/commands/games/plato.ts
@@ -0,0 +1,53 @@
+import { Message } from "discord.js";
+import Command from "../../types/structures/Command";
+import Util from "../../Util";
+
+const command: Command = {
+	name: "plato",
+	aliases: [],
+	description: {
+		fr: "Gérer les tournois Plato",
+		en: "Manage Plato tournaments",
+	},
+	usage: "",
+	userPermissions: ["ADMINISTRATOR"],
+	botPermissions: [],
+	guildIds: [Util.config.MAIN_GUILD_ID],
+
+	options: {
+		fr: [],
+		en: [],
+	},
+
+	runInteraction: async (interaction, translations) => {
+		interaction.followUp(".");
+	},
+
+	runMessage: async (message, args, translations) => {
+		const GAMES = [
+			"Pool",
+			"Ocho",
+			"Bowling",
+			"Mini Golf",
+			"Skeeball",
+			"Dominos",
+			"Darts",
+			"Bounce",
+			"Plox",
+			"Bataille Navale",
+			"Tir à l'arc",
+			"Basketball",
+			"Rangée de 4",
+			"Démineur",
+			"Cup Pong",
+			"Dots & Boxes",
+			"Go Fish",
+		];
+
+		const game = GAMES[Math.floor(Math.random() * GAMES.length)];
+
+		message.reply(`Le jeu sera **${game}** !`);
+	},
+};
+
+export default command;
