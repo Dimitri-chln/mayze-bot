14103594c6e2ef63a9280bddd590d84516a6b8e3
Dimitri-chln <dimitrichaline4@gmail.com>
2022-11-01 22:44:53 +0100

    .

--- src/message-responses/mudae-reaction.ts
+++ src/message-responses/mudae-reaction.ts
@@ -1,17 +1,18 @@
 import MessageResponse from "../types/structures/MessageResponse";
 import Util from "../Util";
 
-import { TextChannel } from "discord.js";
-
 const messageResponse: MessageResponse = {
 	name: "mudae-reaction",
 	noDM: true,
 
 	run: async (message, translations) => {
+		return; // This functionnality is to be removed
+
 		if (message.author.id !== "432610292342587392" /* Mudae */) return;
 
 		const embed = message.embeds[0];
 		if (!embed || embed.color !== 0xff9d2c) return;
+
 		const imRegex = /\d+ \/ \d+/;
 		if (imRegex.test(embed.footer?.text)) return;
 
